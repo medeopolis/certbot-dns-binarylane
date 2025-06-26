a9824074199d65451b99f7ca61237b44d29e637c
Karl Goetz <karl@medeopolis.au>
2025-06-26 21:06:26 +1000

    Update plugin file with binarylane changes
    This commit is the result of squashing a lot of work in to one package.
    
    The BL class can now be imported, instantiated, and all its methods return
    valid data (when provided valid inputs).
    
    The Authenticatoin class talks to ACME servers and calls BL at the right
    moment to add and remove records.

--- certbot_dns_binarylane.py
+++ certbot_dns_binarylane.py
@@ -1,20 +1,28 @@
+"""DNS Authenticator for BinaryLane"""
 import logging
 from typing import Any, Callable, Optional, Union
 
 from certbot import errors
 from certbot.plugins import dns_common
 from certbot.plugins.dns_common import CredentialsConfiguration
-from zeep import Client, Settings
+
+import requests
 
 logger = logging.getLogger(__name__)
 
 
 class Authenticator(dns_common.DNSAuthenticator):
-    """DNS Authenticator for Synergy Wholesale (using DNS Hosting)."""
+    """DNS Authenticator for BinaryLane managed domains"""
 
-    description = "Obtain certificates using a DNS TXT record (if you are using Synergy Wholesale for DNS)."
+    description = "Obtain certificates using a DNS TXT record from BinaryLane"
 
     def __init__(self, *args: Any, **kwargs: Any) -> None:
+        """ Initialise Authenticator class
+
+        Required options:
+          `name`: name of this plugin; 'binarylane'
+          `config`: TBC
+        """
         super().__init__(*args, **kwargs)
         self.credentials: Optional[CredentialsConfiguration] = None
 
@@ -23,50 +31,54 @@ class Authenticator(dns_common.DNSAuthenticator):
         cls, add: Callable[..., None], default_propagation_seconds: int = 30
     ) -> None:
         super().add_parser_arguments(add, default_propagation_seconds)
-        add("credentials", help="Synergy Wholesale credentials INI file.")
+        add("credentials", help="BinaryLane credentials INI file.")
 
     def more_info(self) -> str:
         return (
             "This plugin configures a DNS TXT record to respond to a dns-01 challenge using "
-            + "the Synergy Wholesale API."
+            + "the BinaryLane API."
         )
 
     @property
     def _provider_name(self) -> str:
-        return "synergy_wholesale"
+        return "binarylane"
 
     def _validate(self, credentials) -> None:
-        if not credentials.conf("api_key"):
+        # Error messages indicated values are in the format dns_[plugin name]_[setting_name]; this seems to be working though
+        if not credentials.confobj['bl']['api-token']:
             raise errors.PluginError(
-                "Missing property in credentials configuration file {0}: {1}".format(
-                    credentials.confobj.filename, "synergy_wholesale_api_key"
-                )
-            )
-
-        if not credentials.conf("reseller_id"):
-            raise errors.PluginError(
-                "Missing property in credentials configuration file {0}: {1}".format(
-                    credentials.confobj.filename, "synergy_wholesale_reseller_id"
+                "Missing property in credentials configuration file {0}: {1}\nPerhaps this isn't a BinaryLane configuration file?".format(
+                    credentials.confobj.filename, "api-token"
                 )
             )
 
     def _setup_credentials(self) -> None:
         self.credentials = self._configure_credentials(
             "credentials",
-            "Synergy Wholesale credentials INI file",
+            "Binarylane CLI configuration file",
             {
-                "reseller_id": "Synergy Wholesale Reseller ID",
-                "api_key": "Synergy Wholesale API key",
+#                 "api-token": "BinaryLane API token",
             },
             self._validate,
         )
 
+    # The certbot docs say to use perform() and cleanup() but DNSAuthenticator has _perform() and _cleanup() we can use instead (which are called by
+    # the others)
+
     def _perform(self, domain, validation_name, validation) -> None:
-        error = self._get_synergy_wholesale_client().add_txt_record(
-            domain,
-            validation_name,
-            validation,
+        error = self._get_binarylane_client().add_txt_record(
+          domain,
+          validation_name,
+          validation,
         )
+        """
+        Performs a dns-01 challenge by creating a DNS TXT record.
+
+        :param str domain: The domain being validated.
+        :param str validation_domain_name: The validation record domain name.
+        :param str validation: The validation record content.
+        :raises errors.PluginError: If the challenge cannot be performed
+        """
 
         if error is not None:
             raise errors.PluginError(
@@ -74,82 +86,122 @@ class Authenticator(dns_common.DNSAuthenticator):
             )
 
     def _cleanup(self, domain, validation_name, validation) -> None:
-        error = self._get_synergy_wholesale_client().del_txt_record(
+        error = self._get_binarylane_client().del_txt_record(
             domain,
             validation,
         )
+        """
+        Deletes the DNS TXT record which would have been created by `_perform_achall`.
+
+        Fails gracefully if no such record exists.
+
+        :param str domain: The domain being validated.
+        :param str validation_domain_name: The validation record domain name.
+        :param str validation: The validation record content.
+        """
 
         if error is not None:
             logger.warn("Unable to find or delete the DNS TXT record: %s", error)
 
-    def _get_synergy_wholesale_client(self) -> "_SynergyWholesale":
+    # I don't need to implement get_chall_pref; this is provided by DNSAuthenticator as we only have one option
+
+    def _get_binarylane_client(self) -> "_BinaryLane":
+        """Create instance of BL API client for use during Authentication"""
         if not self.credentials:
             raise errors.Error("Plugin has not been configured.")
-        return _SynergyWholesale(
-            self.credentials.conf("reseller_id"), self.credentials.conf("api_key")
+        return _BinaryLane(
+            self.credentials.confobj['bl']['api-token']
         )
 
-
-class _SynergyWholesale:
+class _BinaryLane:
     """
-    Encapsulates all communication with the Synergy Wholesale API.
+    Encapsulates all communication with the BinaryLane API.
     """
+    request_endpoint = 'domains'
 
-    def __init__(self, reseller_id, api_key) -> None:
-        self.client = Client(
-            "https://api.synergywholesale.com/?wsdl", settings=Settings()
-        )
-
-        self.base_request = {
-            "resellerID": reseller_id,
-            "apiKey": api_key,
+    def __init__(self, api_token) -> None:
+        self.api_token = api_token
+        self.client = requests.Session()
+        self.binarylane_api_endpoint = "https://api.binarylane.com.au/v2"
+        self.binarylane_api_auth_header = {
+          "Authorization": "Bearer {0}".format(self.api_token),
         }
 
-    def add_txt_record(self, domain, validation_name, validation) -> Union[str, None]:
-        request_data = {
-            "domainName": domain,
-            "recordName": validation_name,
-            "recordType": "TXT",
-            "recordContent": validation,
-            "recordTTL": 300,
-            "recordPrio": 0,
+    def domainsplitter(self, full_domain):
+      """Split domain in to parent and subdomains
+
+      BinaryLane API requires the registered domain for its API interactions
+      so we have to extract that from our FQDN.
+
+      Returns tuple of (domain, subdomain)
+      """
+
+      # FIXME: do this better
+      # extract the parent domain
+      parentdomain = '.'.join(full_domain.split(sep='.')[-2:])
+      # And extract the subdomain
+      subdomain = full_domain.removesuffix('.' + parentdomain)
+
+      return parentdomain, subdomain
+
+
+    def add_txt_record(self, fullDomain, validation_name, validation) -> Union[str, None]:
+      """Add txt records using API
+
+      fullDomain is the domain being authenticated, eg www.example.com or demo.example.net; unused in tihs function
+      validation_name is the FQDN used for validation, eg _acme-challenge.www.example.com
+      validation is the content of the TXT record which will be created named validation_name.
+      """
+
+      domain, subdomain = self.domainsplitter(validation_name)
+
+      request_post_data = {
+        "type": "TXT",
+        "name": "{0}".format(subdomain),
+        "data": "{0}".format(validation),
         }
 
-        request_data.update(self.base_request)
+      response = self.client.request('POST', '{0}/{1}/{2}/{3}'.format(self.binarylane_api_endpoint, self.request_endpoint, domain, 'records'),
+          headers=self.binarylane_api_auth_header, json=request_post_data)
 
-        response = self.client.service.addDNSRecord(request_data)
+      return None if response.ok else response.reason
 
-        return None if response["status"] == "OK" else response["errorMessage"]
+    def del_txt_record(self, fullDomain, validation) -> Union[str, None]:
+        """Delete records using API
 
-    def del_txt_record(self, domain, validation) -> Union[str, None]:
-        id = self.find_txt_record_id(domain, validation)
+        Unlike add_txt_record, this doesn't know which type of record is being removed and will remove anything find_txt_record_id says it should.
+
+        domain is the top level domain, eg example.com
+        validation is the content of the TXT record which will be removed.
+        """
+
+        domain, subdomain = self.domainsplitter(fullDomain)
+
+        # Unfortunately removing is a two step process - list all the records, then delete the one we want
+        validation_name_id = self.find_txt_record_id(domain, validation)
 
         # If we failed to get id, return early
-        if id is None:
+        if validation_name_id is None:
             return "Failed to find record"
 
-        request_data = {
-            "domainName": domain,
-            "recordID": id,
-        }
+        response = self.client.request('DELETE', '{0}/{1}/{2}/records/{3}'.format(self.binarylane_api_endpoint, self.request_endpoint, domain,
+              validation_name_id ), headers=self.binarylane_api_auth_header)
 
-        request_data.update(self.base_request)
-        response = self.client.service.deleteDNSRecord(request_data)
-
-        return None if response["status"] == "OK" else response["errorMessage"]
+        # Expected results are 200 and 204 but leaving this open to others.
+        return None if response.ok else response.reason
 
     def find_txt_record_id(self, domain, validation) -> Union[str, None]:
-        request_data = {"domainName": domain}
+        """Helper function to locate specific record in list"""
 
-        request_data.update(self.base_request)
-
-        response = self.client.service.listDNSZone(request_data)
+        response = self.client.request('GET', '{0}/{1}/{2}/{3}'.format(self.binarylane_api_endpoint, self.request_endpoint, domain,
+              'records?type=txt'), headers=self.binarylane_api_auth_header)
 
         # If we failed to get the zone, return early
-        if response["status"] != "OK":
+        if response.status_code > 200:
             return None
 
-        for record in response["records"]:
-            if record["type"] == "TXT" and record["content"] == validation:
+        for record in response.json()["domain_records"]:
+            if record["type"] == "TXT" and record["data"] == validation:
                 return record["id"]
         return None
+
