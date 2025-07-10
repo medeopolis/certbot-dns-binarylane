4adf6ebae891d650b4b230b9c5c2ecc180c767f2
Karl Goetz <karl@medeopolis.au>
2025-07-10 15:11:56 +1000

    Add pytest file with basic tests

---
+++ tests/test_certbot_dns_binarylane.py
@@ -0,0 +1,16 @@
+
+import pytest
+
+# Will need this later for testing configs, io, network requests
+# import mock
+
+# Import smoke test
+from certbot_dns_binarylane import Authenticator
+from certbot_dns_binarylane import _BinaryLane
+
+def test_cls_authenticator_inits():
+  cdb_auth = Authenticator
+
+def test_cls_binarylane_inits():
+  cdb_bl = _BinaryLane
+
